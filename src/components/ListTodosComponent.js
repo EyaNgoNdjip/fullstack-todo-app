a512e5a226851345905c69f0a39bb50e80f3b125
Eya Ngo Ndjip <rosendjip@MacBook-Air-de-Rose.local>
2023-02-09 11:14:17 +0100

    Create ListTodosComponent && ListTodosPage

---
+++ src/components/ListTodosComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h1 className="error"> We are working really hard</h1>
+      <div>
+        Apologies for the 404, We will fix that the issue very quickly !
+      </div>
+    </div>
+  );
+};
+
+export default ErrorComponent;
